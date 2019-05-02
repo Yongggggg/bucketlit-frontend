7a4f99a92826e1d5ff4763d5c8766787beba72c7
Yongggggg <ngyong09@gmail.com>
2019-05-02 20:31:21 +0800

    Login and Register Form Added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import './App.css';
 import HomePage from './pages/HomePage';
 import BucketList from './pages/BucketList';
 import { Route } from "react-router-dom";
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
   return (
     <>
       <Route path="/" component={HomePage} />
-      <Route path="/bucketlist" component={BucketList} />
+      <Route path="/bucketlist/" component={BucketList} />
+      <Route path="/register/" component={Register} />
+      <Route path="/login/" component={Login} />
     </>
   );
 }
